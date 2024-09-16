ce2ede829f52db0ff038149b7c7d8ed544a8a893
soates6 <soates6@charlotte.edu>
2024-09-15 20:51:58 -0400

    User Login

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 #rooms = [
@@ -9,6 +10,16 @@ from .forms import RoomForm
 #    {'id':2, 'name':'Design with me'},
 #    {'id':3, 'name':'Frontend developers'},
 #]
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
